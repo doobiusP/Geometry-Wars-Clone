8ad40fd75673713cd389a1625cb01772ff538a8c
aaryan <aaryanpatil02@gmail.com>
2024-06-20 12:42:21 +0530

    Premake w/ build dir

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "HelloWorld"
+   location "build"
+   configurations { "Debug", "Release" }
+
+project "HelloWorld"
+   location "build/HelloWorld"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
