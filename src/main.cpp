ebbc81063373a2874b36bf368cddc45d2224d732
aaryan <aaryanpatil02@gmail.com>
2024-07-06 14:50:53 +0530

    Commit before remote push

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -44,45 +44,7 @@ int main(int argc, char *argv[])
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(vMin, vMax);
     int randVert = dis(gen);
-    // int n;
-    // float r;
-    // std::cin >> n >> r;
 
-    // std::vector<float> pentagon;
-    // std::vector<unsigned int> indices;
-    // generatePolygon(pentagon, indices, n, r);
-
-    // // for (int i = 0; i < pentagon.size(); ++i)
-    // // {
-    // //     std::cout << pentagon[i] << ", ";
-    // //     if ((i + 1) % 2 == 0)
-    // //         std::cout << '\n';
-    // // }
-    // // std::cout << "\n";
-    // // for (int i = 0; i < indices.size(); ++i)
-    // // {
-    // //     std::cout << indices[i] << ",";
-    // //     if ((i + 1) % 3 == 0)
-    // //         std::cout << '\n';
-    // // }
-
-    // GLuint ebo, vbo, vao;
-    // glGenBuffers(1, &ebo);
-    // glGenBuffers(1, &vbo);
-    // glGenVertexArrays(1, &vao);
-
-    // glBindVertexArray(vao);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    // glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-    // glBufferData(GL_ARRAY_BUFFER, pentagon.size() * sizeof(float), &pentagon[0], GL_STATIC_DRAW);
-
-    // glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
-    // glEnableVertexAttribArray(0);
-
-    // Shader simple = Shader("shaders/polyplace.glsl", "shaders/polyfill.glsl");
-    // simple.use();
     double lastTime = glfwGetTime();
     double currTime;
 
@@ -93,7 +55,7 @@ int main(int argc, char *argv[])
         glClearColor(0.2f, 0.3f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        if (currTime - lastTime >= 5)
+        if (currTime - lastTime >= 2)
         {
             lastTime = currTime;
             randVert = dis(gen);
